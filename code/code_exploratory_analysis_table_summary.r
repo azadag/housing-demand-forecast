46c607917b4cb65724d76a3ed8ec76fd6cdb034f
azadamirghassemi@gmail.com <azadamirghassemi@gmail.com>
2018-11-12 13:37:18 -0800

    Rename table summary file
    Former-commit-id: fb804bf0b6bb450b30422479a42dbc75e4555905
    Former-commit-id: cf8e4d5025d6e5c18c1357d91fed537ba39372a0

--- code/code_exploratory_analysis_table_summary.r
+++ code/code_exploratory_analysis_table_summary.r
@@ -2,12 +2,19 @@ library(plyr)
 library(dplyr)
 library(ggplot2)
 
-ind_data_out
-ind_data_czone
+## using 
+#ind_data_out
+#ind_data_czone
 
 ########################   SUMMARIZE / OUTPUT of TWO Different Household Forecasts  ##########
 ######### OUTPUT total HH income present and forecasts ###########
 ### Graphs and csv data pushed to Household Projections Folder 
+
+#### upto here gives us methodology section 4. ####
+## we can now create a count of number of households by household type 
+## and household income associatged with new job growth
+
+code_ind2 <- read.csv("./Data/_input/2_code_indempBLS.csv")
 
 ### TO DO rename NewHousingCZinc to something about HouseHolds Instead
 HH14 <- data_acs_age2 %>% group_by(czone) %>% summarize(netHH14 = sum(AGE1, na.rm = TRUE))
@@ -63,20 +70,20 @@ names(NewHousingCZ1L) <- c("czone","czonename","variable","HH")
 # NewHousingCZ1L$variable[NewHousingCZ1L$variable == "TotalHH34" ] <- "2034"
 NewHousingCZ1L$forecast <- "Age Industry Forecast"
 
-CaForecast <- rbind(NewHousingCZincL, NewHousingCZ1L)
-CaForecast$forecast <- as.factor(CaForecast$forecast)
-CaForecast$variable <- plyr::mapvalues(CaForecast$variable, from = c("TotalHH14", "TotalHH24", "TotalHH34"), to = c("2014", "2024", "2034"))
-
-p <- ggplot( CaForecast, aes(x = variable, y = log(HH),  group = czonename, color = czonename), log = y) + geom_line() + 
-  geom_point() +  facet_wrap(~ forecast, ncol = 2 , scales = "free") 
-p
-ggsave(plot = p, filename= "./Data/_output/Household Projections/HH_forecast_compare_type.pdf", height = 8, width = 8 )
-
-p <- ggplot( CaForecast, aes(x = variable, y = HH,  group = forecast, color = forecast)) + geom_line() + 
-  geom_point() +  facet_wrap(~ czonename, ncol = 2 , scales = "free") 
-p
-ggsave(plot = p, filename= "./Data/_output/Household Projections/HH_forecast_compare_area.pdf", height = 12, width = 9 )
-
+### comparison of forecasts
+# CaForecast <- rbind(NewHousingCZincL, NewHousingCZ1L)
+# CaForecast$forecast <- as.factor(CaForecast$forecast)
+# CaForecast$variable <- plyr::mapvalues(CaForecast$variable, from = c("TotalHH14", "TotalHH24", "TotalHH34"), to = c("2014", "2024", "2034"))
+# 
+# p <- ggplot( CaForecast, aes(x = variable, y = log(HH),  group = czonename, color = czonename), log = y) + geom_line() + 
+#   geom_point() +  facet_wrap(~ forecast, ncol = 2 , scales = "free") 
+# p
+# ggsave(plot = p, filename= "./Data/_output/Household Projections/HH_forecast_compare_type.pdf", height = 8, width = 8 )
+# 
+# p <- ggplot( CaForecast, aes(x = variable, y = HH,  group = forecast, color = forecast)) + geom_line() + 
+#   geom_point() +  facet_wrap(~ czonename, ncol = 2 , scales = "free") 
+# p
+# ggsave(plot = p, filename= "./Data/_output/Household Projections/HH_forecast_compare_area.pdf", height = 12, width = 9 )
 
 write.csv( NewHousingCZ1L, "./Data/_output/Household Projections/HH_forecast_from_income&industry.csv")
 
@@ -139,17 +146,17 @@ CzoneIndustry$forecast <- as.factor(CzoneIndustry$forecast)
 write.csv( CzoneIndustry, "./Data/_output/Household Projections/HH_forecast_from_area&industry.csv")
 
 
-
-
 #############################################################################
 ############################ industry demand change... ######################
 
-ind_data_czone %>% ungroup() %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034)
+# ind_data_czone %>% ungroup() %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034)
 
 ZpctCH <- ind_data_czone %>% group_by(Ind_Code) %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034) %>%
      mutate(pctch = round((fcast_2024 - fcast_2014)/ fcast_2024, 2), pctch34 = round((fcast_2034 - fcast_2024)/ fcast_2034, 2)) %>%
-     arrange(desc(pctch))
-write.csv(ZpctCH, "./Data/_output/Industry_Projections/Industry Projections/EMPLOYMENT_forecast_changebyIndustry.csv")
+     arrange(desc(pctch)) %>% left_join(code_ind2, by="Ind_Code")
+
+write.csv(ZpctCH, "./Data/_output/Industry_Projections/EMPLOYMENT_forecast_changebyIndustry.csv")
+
 
 # head(Zind_data_czone)
 Zind_data_czone <- ind_data_czone %>% group_by(czone) %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034)
@@ -157,7 +164,8 @@ Zind_data_czone$czone <- as.character(Zind_data_czone$czone)
 Zind_data_czone <- inner_join( Zind_data_czone, code_czone , by=c("czone"="czone"))
 Zind_data_czone$name <- strtrim(Zind_data_czone$czonename, 15 )
 Zind_data_czone$czonename <- NULL
-write.csv( Zind_data_czone, "./Data/_output/CA/Employment/EMPLOYMENT_forecast_byArea.csv")
+write.csv( Zind_data_czone, "./Data/_output/Industry_Projections/EMPLOYMENT_forecast_byArea.csv")
+
 
 Z2ind_data_czone <- ind_data_czone #%>% group_by(czone) %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034)
 Z2ind_data_czone$czone <- as.character(Z2ind_data_czone$czone)
@@ -167,22 +175,28 @@ Z2ind_data_czone$czonename <- NULL
 Z2ind_data_czone$cty_pop <- NULL
 Z2ind_data_czone$cty_labf <- NULL
 Z2ind_data_czone$join <- NULL
-write.csv( Z2ind_data_czone, "./Data/_output/CA/Employment/EMPLOYMENT_forecast_byIndustryArea.csv")
+
+# code_ind2 <- code_ind
+code_ind2$Ind_Code <- as.factor(code_ind2$Ind_Code)
+Z2ind_data_czone1 <- Z2ind_data_czone %>% left_join(code_ind2, by="Ind_Code")
+
+write.csv( Z2ind_data_czone, "./Data/_output/Industry_Projections/EMPLOYMENT_forecast_byIndustryArea.csv")
 
 p <- filter(Z2ind_data_czone)  %>% ggplot(  aes(x = Ind_Code, y = EmpCh14_24, colour = Ind_Code, group = Ind_Code)) + geom_bar(stat = "identity") +
   facet_wrap(~ name, ncol = 2 , scales = "free")  + theme_bw() #+ scale_colour_brewer( Set3)
+
 
 Z2ind_data_czoneL <- Z2ind_data_czone %>% reshape2::melt( id = c("czone","Ind_Code", "name" ), value = "Employment", 6:7)
 
 p <- filter(Z2ind_data_czoneL)  %>% ggplot(  aes(x = Ind_Code , y = value, fill = variable, colour = variable, group = variable)) + geom_bar(position = "dodge", width = .5, stat = "identity") +
   facet_wrap(~ name, ncol = 2 , scales = "free")  + theme_bw() #+ scale_colour_brewer( Set3)
-ggsave("./Data/_output/CA/Employment/EMPLOYMENT_ChangeIndustryByArea.pdf",  height = 14, width = 12)
+ggsave("./Data/_output/CA/Industry_Projections/EMPLOYMENT_ChangeIndustryByArea.pdf",  height = 14, width = 12)
 
 p <- filter(Z2ind_data_czone)  %>% group_by(Ind_Code) %>% summarise_each( funs(sum),EmpCh14_24,EmpCh24_34 ) %>% reshape2::melt( id = c("Ind_Code" ), value = "Employment", 2:3) %>%
   ggplot(  aes(x = Ind_Code , y = value/1e04, fill = variable, colour = variable, group = variable)) + geom_bar(position = "dodge", width = .5, stat = "identity") + theme_bw() 
-p
+
   # facet_wrap(~ name, ncol = 2 , scales = "free")  + theme_bw() #+ scale_colour_brewer( Set3)
-ggsave("./Data/_output/CA/Employment/EMPLOYMENT_ChangeByIndustryCAL.pdf",  height = 10, width = 12)
+ggsave("./Data/_output/Industry_Projections/EMPLOYMENT_ChangeByIndustryCAL.pdf",  height = 10, width = 12)
 
 
 
@@ -222,19 +236,177 @@ Zdata_acs_11hCITYTYPE <- Zdata_acs_11hA %>% group_by(czone, HHTYPE) %>% summaris
          HousingChPct_2434 = round((Housing34Total - Housing24Total)/Housing34Total, 3)  ) %>%
   inner_join( code_czone, by=c("czone"))
 
+Zdata_acs_11hCITYTYPE$czone <- as.factor(Zdata_acs_11hCITYTYPE$czone)
+Zdata_acs_11hCITYTYPE$HHTYPE <- as.factor(Zdata_acs_11hCITYTYPE$HHTYPE)
+
+Zdata_acs_11hCITYTYPE$Housing24Total <- round(Zdata_acs_11hCITYTYPE$Housing24Total,1)
+Zdata_acs_11hCITYTYPE$Housing34Total <- round(Zdata_acs_11hCITYTYPE$Housing34Total,1)
+
+Zdata_acs_11hCITYTYPE$HousingCh_1424 <- round(Zdata_acs_11hCITYTYPE$HousingCh_1424,1)
+Zdata_acs_11hCITYTYPE$HousingCh_2434 <- round(Zdata_acs_11hCITYTYPE$HousingCh_2434,1)
+
+Zdata_acs_11hCITYTYPE$HousingChPct_1424 <- round(Zdata_acs_11hCITYTYPE$HousingChPct_1424,3)
+Zdata_acs_11hCITYTYPE$HousingChPct_2434 <- round(Zdata_acs_11hCITYTYPE$HousingChPct_2434,3)
+
 write.csv( Zdata_acs_11hCITYTYPE, "./Data/_output/Housing Projections/HOUSING_CityTypeTotal.csv")
+
+
+Zdata_acs_11hCITYTYPEwide <- reshape2::melt(Zdata_acs_11hCITYTYPE %>% select(czone, HHTYPE, Housing14Total, Housing24Total, Housing34Total))
+Zdata_acs_11hCITYTYPEwide1 <- reshape2::dcast(Zdata_acs_11hCITYTYPEwide, czone ~ HHTYPE + variable)
+
+#write.csv( Zdata_acs_11hCITYTYPEwide1, "./Data/_output/quip_output/Table_4_housing_type_demandWIDE.csv")
+
+
+# Zdata_acs_11hCITYTYPEwide <- Zdata_acs_11hCITYTYPE %>% select(czone, HHTYPE, Housing14Total) %>% spread(HHTYPE, Housing14Total) %>% 
+    # gather(czone, 2:4)
+# Zdata_acs_11hCITYTYPEwidea <- Zdata_acs_11hCITYTYPEwide %>% gather(czone, HHTYPE)
 ################
 
 
-Zdata_acs_11hTYPE <- Zdata_acs_11hA %>% group_by( HHTYPE) %>% summarise_each(funs(sum), Housing14Total, Housing24Total, Housing34Total) %>% 
+Zdata_acs_11hTYPE <- Zdata_acs_11hA %>% group_by( HHTYPE) %>% summarise_each(funs(sum), Housing14Total, Housing24Total, Housing34Total, HH24new, HH34new) %>% 
   mutate(HousingCh_1424 = Housing24Total - Housing14Total, HousingCh_2434 = Housing34Total - Housing24Total, 
          HousingChPct_1424 = round((Housing24Total - Housing14Total) / Housing24Total, 3), 
-         HousingChPct_2434 = round((Housing34Total - Housing24Total)/Housing34Total, 3)  ) #%>%
+         HousingChPct_2434 = round((Housing34Total - Housing24Total)/Housing34Total, 3),
+         StockPct14 = round((Housing14Total/sum(Housing14Total)),3),
+         StockPct24 = round((Housing24Total/sum(Housing24Total)),3),
+         StockPct34 = round((Housing34Total/sum(Housing34Total)),3),
+         StockNewWorkers24 = round((HH24new/sum(HH24new)),3),
+         StockNewWorkers34 = round((HH34new/sum(HH34new)),3)
+         ) #%>%
   #inner_join( code_czone, by=c("czone"))
 
 write.csv( Zdata_acs_11hTYPE, "./Data/_output/Housing Projections/HOUSING_TypeTotal.csv")
 
+#write.csv( Zdata_acs_11hTYPE, "./Data/_output/quip_output/Table_5_CA_housing_stock_type.csv")
 
+### SUMMARIZE PCTS HERE AND WRITE OUT FOR TABLE 5
 
 # Z2ind_data_czone$czone <- as.character(Z2ind_data_czone$czone)
 
+Zdata_acs_11hTYPEcity <- Zdata_acs_11hA %>% group_by(czone, HHTYPE) %>% summarise_each(funs(sum), Housing14Total, Housing24Total, Housing34Total, HH24new, HH34new) %>% 
+  mutate(HousingCh_1424 = Housing24Total - Housing14Total, HousingCh_2434 = Housing34Total - Housing24Total, 
+         HousingChPct_1424 = round((Housing24Total - Housing14Total) / Housing24Total, 3), 
+         HousingChPct_2434 = round((Housing34Total - Housing24Total)/Housing34Total, 3),
+         StockPct14 = round((Housing14Total/sum(Housing14Total)),3),
+         StockPct24 = round((Housing24Total/sum(Housing24Total)),3),
+         StockPct34 = round((Housing34Total/sum(Housing34Total)),3),
+         StockNewWorkers24 = round((HH24new/sum(HH24new)),3),
+         StockNewWorkers34 = round((HH34new/sum(HH34new)),3)
+  ) %>% inner_join( code_czone,by=c("czone"="czone"))
+
+
+write.csv( Zdata_acs_11hTYPEcity, "./Data/_output/quip_output/Table_5_city_housing_stock_type.csv")
+
+
+## code_new_output.r
+
+library(dplyr)
+library(tidyr)
+
+### 2013 table 1
+ind_data_czone_tot <- ind_data_czone %>% group_by(czone) %>% summarize_each(funs(sum), fcast_2014,fcast_2024,fcast_2034)
+head(ind_data_czone_tot)
+ind_data_czone_tot <- ind_data_czone_tot %>% select(-Ind_Code, - cty_pop, -cty_labf, -join)
+ind_data_czone_tot %>% group_by(czone) %>% mutate_each(funs (round = . * 1000))
+ind_data_czone_tot$czone <- as.character(ind_data_czone_tot$czone)
+ind_data_czone_tot <- inner_join( ind_data_czone_tot, code_czone, by="czone")
+write.csv( ind_data_czone_tot, "./Data/_output/Housing Projections/HOUSING_forecast_bytype.csv")
+
+
+ind_data_czone %>% ungroup() %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034)
+
+ZpctCH <- ind_data_czone %>% group_by(Ind_Code) %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034) %>%
+  mutate(pctch = round((fcast_2024 - fcast_2014)/ fcast_2024, 2), pctch34 = round((fcast_2034 - fcast_2024)/ fcast_2034, 2)) %>%
+  arrange(desc(pctch))
+write.csv(ZpctCH, "./Data/_output/Industry_Projections/EMPLOYMENT_forecast_changebyIndustry.csv")
+
+# head(Zind_data_czone)
+Zind_data_czone <- ind_data_czone %>% group_by(czone) %>% summarise_each( funs(sum),fcast_2014,fcast_2024,fcast_2034)
+Zind_data_czone$czone <- as.character(Zind_data_czone$czone)
+Zind_data_czone <- inner_join( Zind_data_czone, code_czone , by=c("czone"="czone"))
+Zind_data_czone$name <- strtrim(Zind_data_czone$czonename, 15 )
+Zind_data_czone$czonename <- NULL
+write.csv( Zind_data_czone, "./Data/_output/Industry_Projections/EMPLOYMENT_forecast_byArea.csv")
+
+
+######## Table 6 permit data #############
+# str(Ind_EmpTest)
+# str(data_permit)
+
+data_permit <- read.csv("./Data/_input/permit_data/1_cirp_permits.csv")
+data_permit2 <- reshape2::melt(data_permit)
+names(data_permit2) <- c("Date", "Code", "Perm_val")
+
+code_perm    <- read.csv("./Data/_input/permit_data/2_code_permit.csv", nrows=16)
+code_county2 <- read.csv("./Data/_input/permit_data/3_code_county_new.csv", stringsAsFactors = FALSE, colClasses = c("character", "character", "character")) #, nrows=58)
+
+code_ind2 <- read.csv("./Data/_input/2_code_indempBLS.csv")
+
+
+# code_czone <- read.csv("./Data/_input/9_czonename.csv", stringsAsFactors = FALSE, nrows=14)
+# code_czone$czone <- as.character(code_czone$czone)
+
+#### Read and clean employment data ####
+# splitperm <- stringr::str_split(data_permit2$Code, pattern="_", 2)
+splitperm <- stringr::str_split_fixed(data_permit2$Code, "_", 2 )
+splitperma <- as.data.frame(splitperm)
+# splitperm2 <- as.data.frame( t(as.data.frame(splitperm)) )
+# row.names(splitperm2) <- NULL
+names(splitperma) <- c("Perm_code", "county_code_moodies")
+data_permit3 <- cbind(data_permit2, splitperma)
+splitperm <- NULL
+
+# head(data_permit3)
+# library(lubridate)
+# data_permit3$date <- as.Date(data_permit3$Date, "%m/%d/%Y")
+data_permit3$date <- zoo::as.yearmon(data_permit3$Date, "%m/%d/%Y")
+data_permit3$date1 <- zoo::as.Date(data_permit3$Date, "%m/%d/%Y")
+data_permit3$Year <- year(data_permit3$date)
+
+# qplot(data = data_permit3 %>% filter( Perm_code == "XHPIN1"),
+#       x = as.numeric(Date), y = Perm_val, geom = "line", color = county_code_moodies)
+# 
+# data_permit3$Year <- as.numeric(stringr::str_sub(data_permit3$Date, start = 5, end = 8))
+library(lubridate)
+# data_permit3$Year <- year(data_permit3$Date)
+data_permit3$county_code_moodies <- as.character(data_permit3$county_code_moodies)
+data_permit3$Date <-NULL
+data_permit3$Code <-NULL
+code_czone_p <- code_czone
+code_czone_p$czone <- as.integer(code_czone_p$czone)
+
+
+data_permit4 <- data_permit3 %>% select(Year,Perm_code, county_code_moodies,  Perm_val)  %>% 
+  mutate(county_code_moodies2 = as.character(county_code_moodies)) %>% 
+  left_join(code_county2, by = c("county_code_moodies2" )) %>% 
+  left_join(cwcty, by = c("county_code_moodies.y" = "cty_fips")) %>% 
+  left_join( code_czone_p, by=c("czone")) %>% group_by(Year, Perm_code, czonename) %>% 
+  summarise( year_perm = sum(Perm_val)) %>% left_join(code_perm, bydata_permitCAs = c( "Perm_code" = "Mnemonic"))
+
+date_permitCity <- data_permit4 %>% ungroup() %>% 
+  select(-Perm_code) %>%  spread( Code, year_perm) %>% arrange(czonename, Year)
+write.csv( date_permitCity, "./Data/_output/quip_output/Table_6_City_permits.csv")
+
+
+data_permitCA <- data_permit3 %>% select(Year, Perm_code, Perm_val) %>% 
+  filter(Year > 1989) %>% group_by(Year, Perm_code) %>% 
+  summarise(year_perm = sum(Perm_val)) %>% 
+  left_join(code_perm, by = c( "Perm_code" = "Mnemonic")) %>% 
+  select(-Perm_code) %>% spread( Code, year_perm)
+
+
+# write.csv( , "./Data/_output/Housing Projections/HOUSING_forecast_bytype.csv")
+write.csv( data_permitCAs, "./Data/_output/quip_output/Table_6_CA_permits.csv")
+
+ggplot(data = data_permit4 %>% filter(Perm_code == "XHPIN5") ,
+       aes( x = Year, y = year_perm, fill = as.factor(czonename), color = as.factor(czonename)) )+ 
+  geom_bar( stat = "identity" ) + geom_hline(yintercept = 967531/27, color = "grey")  + geom_hline(yintercept = 166317.1)  + 
+  guides(fill = FALSE, color = FALSE)
+
+sum((data_permit4 %>% filter(Perm_code == "XHPIN5"))$year_perm)
+
+Ind_Emp14_34w <- Ind_Emp3 %>% filter(Year == 2014 | Year == 2024 | Year == 2034 ) %>% spread(Year, Empvalue)
+Ind_Emp14_34wl <- Ind_Emp14_34w %>% 
+  left_join(code_perm, by = c( "Ind_code" = "Mnemonic")) %>% 
+  left_join(code_county, by = "county_code_moodies")
+tbl_df(Ind_Emp14_34wl)
